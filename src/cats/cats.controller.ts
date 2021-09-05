01e50ffeb3ec2dae3bdb16e248ca82ade0f30b4b
pthomas0309 <preston.thomas355@gmail.com>
2021-09-04 23:50:11 -0500

    add route param notes

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -3,6 +3,7 @@ import { Controller, Get, Post, Put, Delete, HttpCode, Req, Res, Param, Patch, A
 
 // @Controller decorator defines a basic controller (required)
 // controller prefix is 'cats' (optional)
+// can take a host option to require the host to match a value
 @Controller('cats')
 export class CatsController {
 
@@ -25,7 +26,7 @@ export class CatsController {
     // method returns primitive Javascript types as unserialized values
     // and objects and arrays are returned as serialized JSON
     // the method returns a 200 OK and the string 'This action returns all cats'
-    findAll(): string {
+    /* async tag makes this an asyncronous function */ findAll(): string /* async functions must return a promise: Promise<any[]> */ {
         /* including @Req() decorator tells Nest to inject express request object in handler @Req() request: Request */
         /* including optional @Res decorator in the handler instructs Nest to inject Express response object  @Res() response: Response */ 
         /* other injectable decorators 
@@ -38,6 +39,30 @@ export class CatsController {
         */
         
         return 'This action returns all cats';
+    }
+
+    /* Route Parameters */
+
+    // :id is a parameter token or a dynamic value
+    // that makes a path to the data
+    @Get(':id')
+
+    // handler: GET /cats/:id
+
+    // returns 200 code
+
+    // @Param() decorator in the method signature
+    // this makes the route parameters available
+    // as properties of the decorated method parameter
+    // (in this case params)
+    findOne(@Param() params): string {
+
+        // log the id
+        console.log(params.id);
+
+        // request returns this string with the id targeted in the params
+        return `This action returns a cat with the id of ${params.id}`;
+        
     }
 
     // request method decorator POST
