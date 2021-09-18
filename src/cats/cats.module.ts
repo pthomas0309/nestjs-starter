dde21d42962b8e1bf875bb7a8db8920dac883cff
pthomas0309 <preston.thomas355@gmail.com>
2021-09-17 19:19:47 -0500

    add cats feature module

---
+++ src/cats/cats.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { CatsController } from './cats.controller';
+import { CatsService } from './cats.service';
+
+/* This is a Feature Module,
+a module that groups together files that 
+have the same application domain, 
+ex. User data would me grouped in a feature module
+and exported to the app module*/
+
+@Module({
+    controllers: [CatsController],
+    providers: [CatsService],
+    /* Modules are shared by default, using the exports array
+    we can share an instance of the CatsService accross all modules */
+    exports: [CatsService]
+})
+export class CatsModule {}
