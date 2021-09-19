54070d6e35902549b71ff6aa56630a9e32d25cd3
pthomas0309 <preston.thomas355@gmail.com>
2021-09-18 23:51:47 -0500

    begin custom validation pipe notes

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -9,38 +9,55 @@ import { Cat } from "./interfaces/cat.interface";
 // CLI controller generation command $ nest g controller cats
 
 
-// @Controller decorator defines a basic controller (required)
-// controller prefix is 'cats' (optional)
-// can take a host option to require the host to match a value
+/* @Controller decorator defines a basic controller (required)
+controller prefix is 'cats' (optional) can take a host option
+to require the host to match a value */
 @Controller('cats')
 export class CatsController {
 
-    // The CatsService is injected through this constructor
-    // The use of the private syntax here allows for declaring
-    // and initializing the CatsService in the same location
+    /* 
+    The CatsService is injected through this constructor
+
+    The use of the private syntax here allows for declaring
+    and initializing the CatsService in the same location */
     constructor(private catsService: CatsService) {}
 
-    // request method decorator
-    // creates a handler for an endpoint
-    // the route path determined by concatonating
-    // the controller prefix ('cats') and the
-    // path determined in the decorator
+    /* request method decorator
+    creates a handler for an endpoint
+    the route path determined by concatonating 
+    controller prefix ('cats') and the
+    path determined in the decorator */
     @Get()
 
-    // the handler corresponds to request method (GET)
-    // and the route path
+    
+    // the handler corresponds to request method (GET) and the route path
+
     // handler: GET /cats
 
-    // HttpCode controls status code returned
-    // default 200 (201 for POST)
-    // @HttpCode(204)
+    /* 
+    HttpCode controls status code returned
 
-    // findAll() is a user defined method (effectively taco())
-    // method returns primitive Javascript types as unserialized values
-    // and objects and arrays are returned as serialized JSON
-    // the method returns a 200 OK and the string 'This action returns all cats'
-    /* async tag makes this an asyncronous function */ 
-    async findAll(@Query() query: ListAllEntities): Promise<Cat[]> /* async functions must return a promise: Promise<any[]> */ {
+    default 200 (201 for POST)
+
+    @HttpCode(204) 
+    */
+
+    /* 
+    findAll() is a user defined method (effectively taco())
+
+    method returns primitive Javascript types as unserialized values
+    and objects and arrays are returned as serialized JSON
+
+    this method returns a 200 OK and an array of 
+    Cat interface objects from the findAll method 
+    on catsService provider 
+    */
+
+    /* 
+    async tag makes this an asyncronous function 
+    async functions must return a promise ex. Promise<any[]>
+    */ 
+    async findAll(@Query() query: ListAllEntities): Promise<Cat[]> {
         /* including @Req() decorator tells Nest to inject express request object in handler @Req() request: Request */
         /* including optional @Res decorator in the handler instructs Nest to inject Express response object  @Res() response: Response */ 
         /* other injectable decorators 
@@ -61,16 +78,16 @@ export class CatsController {
     // that makes a path to the data
     @Get(':id')
 
-    // handler: GET /cats/:id
+    /* 
+    handler: GET /cats/:id returns 200 code
 
-    // returns 200 code
-
-    // @Param() decorator in the method signature
-    // this makes the route parameters available
-    // as properties of the decorated method parameter
-    // (in this case params)
-    // to use a validation pipe, it must be bound at the method parameter /* pipes will either transfer the input data to the desired form or validate the data */
-    async findOne(@Param('id', ParseIntPipe) catId: number): Promise<Cat[]> { // here the ParseIntPipe insures the method parameter value is transformed to an integer
+    @Param() decorator in the method signature, this makes the route parameters available
+    as properties of the decorated method parameter (in this case params)
+    to use a validation pipe, it must be bound at the method parameter 
+    pipes will either transfer the input data to the desired form or validate the data 
+    here the ParseIntPipe insures the method parameter value is transformed to an integer
+    */
+    async findOne(@Param('id', ParseIntPipe) catId: number): Promise<Cat[]> { 
 
         // log the id
         console.log(catId);
@@ -90,12 +107,16 @@ export class CatsController {
     // request method decorator POST
     @Post('food')
 
-    // handler: POST /cats/food
+    /* 
+    handler: POST /cats/food
     
-    // returns 201 code for POST
+    returns 201 code for POST 
+    */
     
-    // @Body decorates the DTO that we use in the method signature
-    // the DTO defines how the Data is transfered for this post action
+    /* 
+    @Body decorates the DTO that we use in the method signature
+    the DTO defines how the Data is transfered for this post action
+     */
      async create(@Body() catFoodDto: CatFoodDTO) {
         return `This action adds a ${catFoodDto.food} as a food for cat at id, ${catFoodDto.catId}`;
     }
